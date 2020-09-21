0c34a7a07b4496d28a2ad82b5994eb01b696eb07
jobairRafi96 <jubairrafi96960@gmail.com>
2020-09-20 23:50:21 -0700

    login with passportlocal

--- server.js
+++ server.js
@@ -1,23 +1,51 @@
+if(process.env.NODE_ENV!== "production"){
+  require("dotenv").config()
+}
+
 const express = require("express");
-const bcrypt = require("bcrypt");
 const app = express();
+const bcrypt = require("bcrypt");
+const passport = require("passport");
+const flash = require("express-flash");
+const session = require("express-session");
+
+
+const initializePassport = require("./passport-config");
+initializePassport(
+  passport,
+  email=> users.find(user => user.email === email),
+  id=> users.find(user => user.id === id)
+);
+
 
 const users = [];
 
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended:false}));
+app.use(flash())
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave:false,
+  saveUninitialized:false
+}));
 
-app.get("/",(req,res)=>{
-  res.render("index");
+app.use(passport.initialize())
+app.use(passport.session())
+
+
+app.get("/",checkAuthenticated,(req,res)=>{
+  res.render("index",{name: req.user.name});
 });
 
 app.get("/login",(req,res)=>{
   res.render("login");
 });
 
-app.post("/login",(req,res)=>{
-
-});
+app.post("/login",passport.authenticate("local",{
+  successRedirect: "/",
+  failureRedirect:"/login",
+  failureFlash: true
+}));
 
 app.get("/register",(req,res)=>{
   res.render("register");
@@ -38,5 +66,13 @@ app.post("/register",async(req,res)=>{
     }
     console.log(users);
 });
+
+function checkAuthenticated(req,res,next){
+  if(req.isAuthenticated()){
+    return next()
+  }
+
+  res.redirect("/login")
+}
 
 app.listen(3000)
