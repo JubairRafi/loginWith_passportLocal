b135e5f514b92d3e8472e59892a07fe39136af55
jobairRafi96 <jubairrafi96960@gmail.com>
2020-09-21 00:10:07 -0700

    logout using method overirde

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const bcrypt = require("bcrypt");
 const passport = require("passport");
 const flash = require("express-flash");
 const session = require("express-session");
+const methodOverride = require("method-override");
 
 
 const initializePassport = require("./passport-config");
@@ -31,27 +32,28 @@ app.use(session({
 
 app.use(passport.initialize())
 app.use(passport.session())
+app.use(methodOverride("_method"))
 
 
 app.get("/",checkAuthenticated,(req,res)=>{
   res.render("index",{name: req.user.name});
 });
 
-app.get("/login",(req,res)=>{
+app.get("/login",checkNotAuthenticated, (req,res)=>{
   res.render("login");
 });
 
-app.post("/login",passport.authenticate("local",{
+app.post("/login",checkNotAuthenticated, passport.authenticate("local",{
   successRedirect: "/",
   failureRedirect:"/login",
   failureFlash: true
 }));
 
-app.get("/register",(req,res)=>{
+app.get("/register",checkNotAuthenticated,(req,res)=>{
   res.render("register");
 });
 
-app.post("/register",async(req,res)=>{
+app.post("/register", checkNotAuthenticated, async(req,res)=>{
     try {
       const hashedPass = await bcrypt.hash(req.body.password, 10); //using bcrypt
       users.push({
@@ -67,12 +69,24 @@ app.post("/register",async(req,res)=>{
     console.log(users);
 });
 
+app.delete("/logout",(req,res)=>{
+  req.logOut()
+  res.redirect("/login")
+})
+
 function checkAuthenticated(req,res,next){
   if(req.isAuthenticated()){
     return next()
   }
 
   res.redirect("/login")
+}
+function checkNotAuthenticated(req,res,next){
+  if(req.isAuthenticated()){
+    return res.redirect("/")
+  }
+   next()
+
 }
 
 app.listen(3000)
